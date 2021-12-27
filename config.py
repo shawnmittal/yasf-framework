2ae78bf81c9b33652a63dca8fc3d41b9fbdb4e0c
Shawn Mittal <shawn@shawnmittal.com>
2021-12-27 10:29:14 -0500

    integrating postgresql testing server

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_CONNECTION_URL') or 'postgresql+psycopg2://postgres:password@localhost:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or None
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or None
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
     ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 25
