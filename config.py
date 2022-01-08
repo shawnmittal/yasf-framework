ed8a758da0eaa9f8fa618e79d676fdf710ec46ce
Shawn Mittal <shawn.mittal@gmail.com>
2022-01-08 09:22:21 -0500

    migrated to blueprints for each app function, added free template, flaskified the template

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_CONNECTION_URL') or 'postgresql+psycopg2://postgres:password@localhost:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB_CONNECTION_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or None
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
@@ -11,4 +15,3 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or None
     ADMINS = ['your-email@example.com']
     LANGUAGES = ['en', 'es']
-    POSTS_PER_PAGE = 25
